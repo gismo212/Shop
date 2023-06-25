8d5038c0c808e0fec0acc96f1cf6434ef3e05a0f
Elegard <whiteandy21321@gmail.com>
2023-06-25 23:46:38 +0300

    create form for contacts view

--- app.rb
+++ app.rb
@@ -4,6 +4,10 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
+set :database, {adapter: "sqlite3", database: "shop.db"}
+
+class Contact < ActiveRecord::Base
+end
 get '/' do
 	erb :home
 end
@@ -12,12 +16,9 @@ get '/contacts' do
 	erb :contacts
 end
 
-
 get '/about' do
 	erb :about
-
 end
-
 
 get '/product' do
 	erb :product
