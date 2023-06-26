a019ba0819d33782690625cd52f9f83b17b51d39
Elegard <whiteandy21321@gmail.com>
2023-06-26 11:13:01 +0300

    add validation for  contacts parameters

--- app.rb
+++ app.rb
@@ -4,14 +4,19 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
+#0 удалить сохрание по умолчанию в contacts?
 #1-добавить отзывы на страницу contacts
 #2-сделать список продуктов(бд) и заполнить ее
-#3-вывести список продуктов с помощью грид
+#3-вывести список продуктов с помощью грид 
 #4-добавить корзину и сделать возможность покупать (local.storage,js)
 
 set :database, {adapter: "sqlite3", database: "shop.db"}
 
 class Contact < ActiveRecord::Base
+	validates :name, presence:  true
+	validates :address, presence:  true
+	validates :email, presence:  true
+	validates :message, presence:  true
 end
 get '/' do
 	erb :home
