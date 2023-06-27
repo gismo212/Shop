f51777c5bfc49617001cfa0e7ce65a16f6649244
Elegard <whiteandy21321@gmail.com>
2023-06-27 20:46:18 +0300

    add button to /product

--- public/b.js
+++ public/b.js
@@ -3,7 +3,29 @@ function con()
     alert('Thanks for your feedback!')
 }
 
-function add_to_cart()
+function add_to_cart(id)
 {
-    alert('hello all')
+  var key = 'product_' + id;
+
+  var x = window.localStorage.getItem(key);
+  x = x * 1 + 1;
+  window.localStorage.setItem(key, x);
+}
+
+function cart_get_number_of_items()
+{
+  var cnt = 0;
+
+  for (var i = 0; i < window.localStorage.length; i++)
+  {
+    var key = window.localStorage.key(i); // получаем ключ
+    var value = window.localStorage.getItem(key); // получаем значение
+
+    if(key.indexOf('product_') == 0)
+    {
+      cnt = cnt + value * 1;
+    }
+  }
+
+  return cnt;
 }
