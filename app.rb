386f7d3dce61d0f7b661e9b9a457f9a368e09d29
Elegard <whiteandy21321@gmail.com>
2023-06-27 21:40:15 +0300

    app.rb update

--- app.rb
+++ app.rb
@@ -22,7 +22,6 @@ end
 class Order < ActiveRecord::Base 
 end
 
-
 get '/' do
 	erb :home
 end
@@ -45,42 +44,66 @@ get '/product' do
 	erb :product
 end
 
-get '/cart' do
-	erb :cart
+post '/place_order' do
+	@order = Order.create params[:order]
+	erb :order_placed
+end
+
+
+get '/admin' do
+	@all = Order.order "created_at DESC"
+	erb :admin
+end
+
+post '/admin' do
+	erb :admin
 end
 
 post '/cart' do
-	orders_input = params[:orders]
-  
-	@items = parse_orders_input orders_input
-  
+	# получаем список параметров и разбираем(parse) их
+	@orders_input = params[:orders_input]
+	@items = parse_orders_input @orders_input
+
+
+	# выводим сообщение о том что корзина пуста
+	if @items.length == 0
+		return erb :cart_is_empty
+	end
+
+	# выводим список продуктов  в корзине
 	@items.each do |item|
-	  # id, cnt
-	  item[0] = Product.find(item[0])
-	end
-  
+
+		item[0] = Product.find(item[0])
+	end 
+
+	#озвращаем представление по-умолчанию
 	erb :cart
-  end
-  
-  def parse_orders_input orders_input
-  
+end
+
+def parse_orders_input orders_input
+
 	s1 = orders_input.split(/,/)
-  
-	arr = []
-  
+
+	arr=[]
+
 	s1.each do |x|
-	  s2 = x.split(/=/)
-	  s3 = s2[0].split(/_/)
-  
-	  id = s3[1]
-	  cnt = s2[1]
-  
-	  arr2 = [id, cnt]
-	  arr.push arr2
+		s2 = x.split(/\=/)
+
+		s3 = (s2[0]).split(/_/)	
+
+
+		id = s3[1]
+
+		cnt = s2[1]
+		
+		arr2 = [id,cnt]
+
+		arr.push arr2
+
 	end
-  
+	
 	return arr
-  end
+end
 
 get '/review' do
 	@contacts = Contact.all
